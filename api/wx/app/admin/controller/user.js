eecb50a05d7067931f0a6734ba408a23d1b23236
root <root@iZ25qrn9204Z.(none)>
2015-12-27 17:26:42 +0800

    test

--- api/wx/app/admin/controller/user.js
+++ api/wx/app/admin/controller/user.js
@@ -77,4 +77,19 @@
 
 
 		datas;return _regeneratorRuntime.async(function scoreAction$(context$2$0) {while (1) switch (context$2$0.prev = context$2$0.next) {case 0:data = this.post();context$2$0.next = 3;return _regeneratorRuntime.awrap(this.session('userInfo'));case 3:userInfo = context$2$0.sent;console.log('userInfo', userInfo); // if(!userInfo) this.fail('请先关注');
-					context$2$0.next = 7;return _regeneratorRuntime.awrap(think.isNumber(data.score));case 7:if (context$2$0.sent) {context$2$0.next = 9;break;}this.fail('参数不正确');case 9:model = this.model('user');if (!data.type) {context$2$0.next = 15;break;}context$2$0.next = 13;return _regeneratorRuntime.awrap(model.where({ id: 50 }).find());case 13:info = context$2$0.sent;data.score += info.user_score;case 15:context$2$0.next = 17;return _regeneratorRuntime.awrap(model.where({ id: 50 }).update({ user_score: data.score }));case 17:datas = context$2$0.sent;this.success('记分成功');case 19:case "end":return context$2$0.stop();}}, null, this);};return _default;})(_baseJs2["default"]);exports["default"] = _default;module.exports = exports["default"];
+					context$2$0.next = 7;return _regeneratorRuntime.awrap(think.isNumber(data.score));case 7:if (context$2$0.sent) {context$2$0.next = 9;break;}this.fail('参数不正确');case 9:model = this.model('user');if (!data.type) {context$2$0.next = 15;break;}context$2$0.next = 13;return _regeneratorRuntime.awrap(model.where({ id: 50 }).find());case 13:info = context$2$0.sent;data.score += info.user_score;case 15:context$2$0.next = 17;return _regeneratorRuntime.awrap(model.where({ id: 50 }).update({ user_score: data.score }));case 17:datas = context$2$0.sent;this.success('记分成功');case 19:case "end":return context$2$0.stop();}}, null, this);};_default.prototype.
+
+	getcodeAction = function getcodeAction() {var 
+		data, 
+		client, 
+		openid, 
+		userInfo;return _regeneratorRuntime.async(function getcodeAction$(context$2$0) {while (1) switch (context$2$0.prev = context$2$0.next) {case 0:data = this.get();client = new OAuth('wxa0bb7dd833ca89ce', '45fa8e4a422764b13cd2510b76eeed6b');openid = undefined;userInfo = undefined;
+					client.getAccessToken(data.code, function (err, result) {
+						var accessToken = result.data.access_token;
+						openid = result.data.openid;});
+
+					client.getUser(openid, function (err, result) {
+						userInfo = result;});
+
+					console.log(data);
+					this.success({ userinfo: userInfo });case 8:case "end":return context$2$0.stop();}}, null, this);};return _default;})(_baseJs2["default"]);exports["default"] = _default;module.exports = exports["default"];
